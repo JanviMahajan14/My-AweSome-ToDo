2a56558b329bbbca2e773141f87b09847d9839d0
JanviMahajan14 <janvimahajan337@gmail.com>
2019-06-06 12:55:41 +0530

    First commit :)

---
+++ script.js
@@ -0,0 +1,84 @@
+// alert("Connected")
+
+
+var addbutton = document.querySelector("#addtodo")
+addbutton.addEventListener("click",addtodo)
+
+
+var removebutton = document.querySelector("#removetodo")
+removebutton.addEventListener("click",removetodo)
+
+
+
+var removeallbutton = document.querySelector("#removealltodo")
+removeallbutton.addEventListener("click",removealltodo)
+
+
+function addtodo () {
+
+    var item = document.querySelector(".enter").value
+    
+    console.log("Add todo is clicked");
+    ul = document.querySelector("ul")
+    if(ul==null){
+        ul = document.createElement("ul")
+        var create =1
+        
+    }
+    li = document.createElement("li")
+    input = document.createElement("input")
+    input.type = "checkbox"
+    input.setAttribute("id","tocheck")
+    label = document.createElement("label")
+    text = document.createTextNode(item)
+
+     
+    label.appendChild(text)
+    li.appendChild(input)
+    li.appendChild(label)
+    ul.appendChild(li)
+
+    if(create == 1){
+        list = document.querySelector(".list")
+        list.appendChild(ul)
+    }
+
+    // to clear the entered input value
+    document.querySelector(".enter").value = ''
+   
+    
+
+
+}
+
+function removetodo () {
+  console.log("Remove todo is clicked");
+  ul= document.querySelector("ul")
+//console.log(ul);
+  li = ul.children
+  
+ 
+  
+for (let index = 0; index < li.length; index++) {
+    const element = li[index]
+    // console.log(element);
+    if(element.children[0] == document.querySelector("#checked") || document.querySelectorAll("#tocheck")[index].checked){
+        element.remove()
+        index--;
+    }
+}
+
+}
+
+
+function removealltodo (e) {
+
+   
+    console.log("Remove all is clicked");
+    ul = document.querySelector("ul")
+    console.log(ul);
+    ul.remove()
+ 
+}
+
+
